ac49a7ba92c7afb403e62a9f6ebe8c48be3a5cee
Salvador Salcedo <salsalcedo4321@gmail.com>
2018-09-25 14:17:05 -0700

    more

--- Project 1 (Part A)/Project 1 (Part A)/MemberType.cpp
+++ Project 1 (Part A)/Project 1 (Part A)/MemberType.cpp
@@ -3,4 +3,37 @@
 #define MEMBERTYPE_CPP
 
 
-#endif // !MEMBERTYPE_CPP
+MemberType::MemberType()
+{
+	int memberNum = 0;
+}
+
+MemberType::MemberType(const string & first, const string & last, int num)
+{
+}
+
+void MemberType::printMemberInfo() const
+{
+}
+
+string MemberType::getFirstName() const
+{
+	return string();
+}
+
+string MemberType::getLastName() const
+{
+	return string();
+}
+
+int MemberType::getMembershipNo() const
+{
+	return 0;
+}
+
+MemberType::~MemberType()
+{
+}
+
+
+#endif
--- Project 1 (Part A)/Project 1 (Part A)/MemberType.h
+++ Project 1 (Part A)/Project 1 (Part A)/MemberType.h
@@ -1,5 +1,33 @@
 #ifndef MEMBERTYPE_H
 #define MEMBERTYPE_H
 
+#include <string>
+#include <iostream>
+
+class MemberType {
+public:
+	//Constructors
+	MemberType();
+	MemberType(const string& first, const string& last, int num);
+
+	//Void
+	void setMemberInfo();
+	void printName() const;
+	void printMemberInfo() const;
+
+	//Return values
+	string getFirstName() const;
+	string getLastName() const;
+	int getMembershipNo() const;
+
+
+	~MemberType();
+private:
+	string firstName;
+	string lastName;
+	int memberNum;
+
+};
+
 #endif // !1
 
