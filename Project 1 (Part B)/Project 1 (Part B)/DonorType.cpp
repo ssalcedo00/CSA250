b63c6e3f1bee7faac6d48ddb18ca4183a54d30c1
Salvador Salcedo <salsalcedo4321@gmail.com>
2018-09-29 13:37:54 -0700

    working

--- Project 1 (Part B)/Project 1 (Part B)/DonorType.cpp
+++ Project 1 (Part B)/Project 1 (Part B)/DonorType.cpp
@@ -1,1 +1,35 @@
+#include "DonorType.h"
 
+DonorType::DonorType()
+{
+}
+
+DonorType::DonorType(const string &, const string &, int, double)
+{
+}
+
+void DonorType::setDonorInfo(const string & newFName, const string & newLName, int newNum, double newAmount) {
+	
+	dAmount = newAmount;
+}
+
+void DonorType::setAmountDonated(double newAmount) {
+	dAmount = newAmount;
+}
+
+double DonorType::getAmountDonated() const
+{
+	return 0.0;
+}
+
+void DonorType::printDonor()
+{
+}
+
+void DonorType::printDonation()
+{
+}
+
+DonorType::~DonorType()
+{
+}
---
+++ Project 1 (Part B)/Project 1 (Part B)/MemberType.cpp
@@ -0,0 +1,63 @@
+
+/*
+Phan, Thang
+Salcedo, Salvador
+Nguyen, Tony
+Kim, Ji Soo
+
+CS A250
+September 29th, 2018
+
+Project 1 Part A
+*/
+
+#include "MemberType.h"
+#include <iostream>
+
+using namespace std;
+
+MemberType::MemberType() {
+	num = 0;
+}
+
+MemberType::MemberType(const string & firstN, const string & lastN, int no)
+{
+	fName = firstN;
+	lName = lastN;
+	num = no;
+}
+
+void MemberType::setMemberInfo(const string & firstN, const string & lastN, int no)
+{
+	fName = firstN;
+	lName = lastN;
+	num = no;
+}
+
+string MemberType::getFirstName() const
+{
+	return fName;
+}
+
+string MemberType::getLastName() const
+{
+	return lName;
+}
+
+int MemberType::getMembershipNo() const
+{
+	return num;
+}
+
+void MemberType::printName() const
+{
+	cout << fName << ", " << lName << endl;
+}
+
+void MemberType::printMemberInfo() const
+{
+	cout << num << " - " << fName << " " << lName << endl;
+}
+
+MemberType::~MemberType() {}
+
