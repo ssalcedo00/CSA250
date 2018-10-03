2b60a09b352ba438e40719ae08beb4027da4aae4
Salvador Salcedo <salsalcedo4321@gmail.com>
2018-10-02 18:06:49 -0700

    commit

--- Project 1 (Part B)/Project 1 (Part B)/Main.cpp
+++ Project 1 (Part B)/Project 1 (Part B)/Main.cpp
@@ -1,6 +1,10 @@
 #include "DonorType.h"
 
 int main() {
+	DonorType DonorType();
+	DonorType man = DonorType("its", "ya boi", 83284239, 235.62);
+
+	man.printDonor();
 
 	system("pause");
 	return 0;
