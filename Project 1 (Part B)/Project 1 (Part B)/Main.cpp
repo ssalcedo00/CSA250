3ea4d24a6b082e844c4165c71d38dca8d9042845
Salvador Salcedo <salsalcedo4321@gmail.com>
2018-10-06 15:24:23 -0700

    use this one Thang

--- Project 1 (Part B)/Project 1 (Part B)/Main.cpp
+++ Project 1 (Part B)/Project 1 (Part B)/Main.cpp
@@ -1,7 +1,7 @@
 #include "DonorType.h"
 
 int main() {
-	DonorType DonorType();
+	DonorType person = DonorType();
 	DonorType man = DonorType("its", "ya boi", 83284239, 235.62);
 
 	man.printDonor();
