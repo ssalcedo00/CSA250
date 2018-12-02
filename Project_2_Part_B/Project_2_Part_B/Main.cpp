0527934e36f0125c1e3b301e7eb283b69b5b9088
Jisoo Kim <jisookim20032003@gmail.com>
2018-12-01 18:03:29 -0800

    Add files via upload

---
+++ Project_2_Part_B/Project_2_Part_B/Main.cpp
@@ -0,0 +1,190 @@
+/*
+	Plumbus
+	Phan, Thang
+	Salcedo, Salvador
+	Nguyen, Tony
+	Kim, Ji Soo
+
+	CS A250
+	December 1, 2018
+
+	Project 2: Donor List
+*/
+
+
+#include "DonorList.h"
+#include "InputHandler.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void displayMenu();
+void processSelection(DonorList& donorList);
+
+const string FILE_NAME = "donors_data.txt";
+
+int main()
+{
+	DonorList donorList;
+
+	readDonorData(donorList);
+	displayMenu();
+	processSelection(donorList);
+
+	cout << endl;
+	system("Pause");
+	return 0;
+}
+
+void displayMenu()
+{
+	cout << "\n*** MAIN MENU ***\n";
+	cout << "\nSelect one of the following:\n\n";
+	cout << "    1. Add a donor" << endl;
+	cout << "    2. Delete a donor" << endl;
+	cout << "    3. Find a donor by membership number" << endl;
+	cout << "    4. Print all donors" << endl;
+	cout << "    5. Print a donor's donation" << endl;
+	cout << "    6. Print total donations" << endl;
+	cout << "    7. Print highest donation" << endl;
+	cout << "    8: To exit" << endl;
+}
+
+void processSelection(DonorList& donorList)
+{
+	cout << "\nEnter your selection: ";
+	int selection = 0;
+	cin >> selection;
+	cout << endl;
+
+	while (selection != 8)
+	{
+		string fName, lName;
+		int memberNo = 0;
+		double donation = 0.0;
+
+		cout << fixed << showpoint << setprecision(2);
+
+		switch (selection)
+		{
+			// Add a donor
+		case 1:
+			cout << "  => Enter donor's first name: ";
+			cin >> fName;
+			fName.at(0) = toupper(fName.at(0));
+			cout << "  => Enter donor's last name: ";
+			cin >> lName;
+			lName.at(0) = toupper(lName.at(0));
+			cout << "  => Enter donor's membership number: ";
+			cin >> memberNo;
+			cout << "  => Enter amount donated: $ ";
+			cin >> donation;
+			cout << endl;
+			/*
+			We are using search here because the addDonor function
+			simply adds a donor at the end of the list without
+			checking if the donor is already in the list.
+			*/
+			if (donorList.searchID(memberNo))
+				cerr << "  => This donor is already in the list." << endl;
+			else
+			{
+				donorList.addDonor(fName, lName, memberNo, donation);
+				cout << "  => Donor has been added." << endl;
+			}
+			break;
+
+			// Delete a donor
+		case 2:
+			if (donorList.isEmpty())
+				cerr << "  => There are no donors in the list." << endl;
+			else
+			{
+				cout << "  => Enter donor's membership number: ";
+				cin >> memberNo;
+				cout << endl;
+				if (!donorList.searchID(memberNo))
+					cerr << "  => This donor is not in the list." << endl;
+				else
+				{
+					donorList.deleteDonor(memberNo);
+					cout << "  => Donor has been deleted." << endl;
+				}
+			}
+			break;
+
+			// Find a donor by membership number
+		case 3:
+			if (donorList.isEmpty())
+				cerr << "  => There are no donors in the list." << endl;
+			else
+			{
+				cout << "  => Enter donor's membership number: ";
+				cin >> memberNo;
+				cout << endl;
+				if (!donorList.searchID(memberNo))
+					cout << "  => Donor not found.\n";
+				else
+					donorList.printDonor(memberNo);
+			}
+			break;
+
+			// Print all donors
+		case 4:
+			if (donorList.isEmpty())
+				cerr << "  => There are no donors in the list." << endl;
+			else
+				donorList.printAllDonors();
+			break;
+
+			// Print a donor's donation
+		case 5:
+			if (donorList.isEmpty())
+				cerr << "  => There are no donors in the list." << endl;
+			else
+			{
+				cout << "  => Enter donor's membership number: ";
+				cin >> memberNo;
+				cout << endl;
+				if (!donorList.searchID(memberNo))
+					cout << "  => Donor not found.\n";
+				else
+					donorList.printDonation(memberNo);
+			}
+			break;
+
+			// Print total donations
+		case 6:
+			if (donorList.isEmpty())
+				cerr << "  => There are no donors in the list." << endl;
+			else
+				donorList.printTotalDonations();
+			break;
+
+			// Print highest donation and donor's information
+		case 7:
+			if (donorList.isEmpty())
+				cerr << "  => There are no donors in the list." << endl;
+			else
+				donorList.printHighestDonation();
+			break;
+
+		default:
+			cout << "  => Sorry. That is not a selection." << endl;
+			break;
+		}
+
+		cout << endl;
+		system("Pause");
+		displayMenu();
+		cout << "\n  => Enter your selection: ";
+		cin >> selection;
+		cout << endl;
+	}
+
+	if (selection == 8)
+		cout << "  => Thank you for visiting our site!" << endl;
+}
---
+++ Project_2_Part_B/Project_2_Part_B/MemberType.cpp
@@ -0,0 +1,61 @@
+/*
+	Plumbus
+	Phan, Thang
+	Salcedo, Salvador
+	Nguyen, Tony
+	Kim, Ji Soo
+
+	CS A250
+	December 1, 2018
+
+	Project 2: Donor List
+*/
+
+#include "MemberType.h"
+
+MemberType::MemberType()
+{
+	num = 0;
+}
+
+MemberType::MemberType(const string& firstN, const string& lastN, int no)
+{
+	fName = firstN;
+	lName = lastN;
+	num = no;
+}
+
+void MemberType::setMemberInfo(const string& firstN, const string& lastN, int no)
+{
+	fName = firstN;
+	lName = lastN;
+	num = no;
+}
+
+string MemberType::getFirstName() const
+{
+	return fName;
+}
+
+string MemberType::getLastName() const
+{
+	return lName;
+}
+
+int MemberType::getMembershipNo() const
+{
+	return num;
+}
+
+void MemberType::printName() const
+{
+	cout << "     " << lName << ", " << fName << endl;
+}
+
+void MemberType::printMemberInfo() const
+{
+	cout << "     " << num << " - " << fName << " " << lName << endl;
+}
+
+MemberType::~MemberType()
+{}
